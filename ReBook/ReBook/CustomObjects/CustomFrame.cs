47b6e608e658466bd62554ad2f70555dc48a7b59
Valdemar-iatneh <avvakumov112003@gmail.com>
2022-03-22 16:28:33 +0300

    Castom Frame added

---
+++ ReBook/ReBook/CustomObjects/CustomFrame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ReBook.CustomObjects
+{
+    public class CustomFrame: Frame
+    {
+        public static new readonly BindableProperty CornerRadiusProperty =
+            BindableProperty.Create(nameof(CustomFrame), typeof(CornerRadius), typeof(CustomFrame));
+        public CustomFrame()
+        {
+            base.CornerRadius = 0;
+        }
+
+        public new CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+    }
+}
