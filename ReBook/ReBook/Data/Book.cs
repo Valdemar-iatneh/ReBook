98dc6ec1c49c4c06bb06aa53ea13a6860640997d
Vladimir Avvakumov <avvakumov112003@gmail.com>
2022-03-23 20:06:04 +0300

    something done /Artem

---
+++ ReBook/ReBook/Data/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using Newtonsoft.Json;
+
+namespace ReBook.Data
+{
+    [Table("Books")]
+    public class Book
+    {
+        [PrimaryKey, Column("_id")]
+        [JsonProperty("build_id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("author")]
+        public string Author { get; set; }
+        [JsonProperty("img_src")]
+        public string Image { get; set; }
+
+    }
+}
